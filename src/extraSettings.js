67df8a57217aadcedffb6fe963895d96982aa3e2
jis127 <jis127@naver.com>
2023-10-15 03:17:25 +0900

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -38,7 +38,7 @@ let clickChangeFruit = true;
 let adLink = '';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '하유수박게임';
+document.getElementsByTagName("title")[0].innerText = 'BingHayu';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
